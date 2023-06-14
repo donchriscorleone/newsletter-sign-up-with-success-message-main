297c179a10ee645e880eb5513e7b5881daea9499
Christopher II Lajom <christopheriilajom@gmail.com>
2023-06-14 22:55:46 +0800

    Initialized.

---
+++ script.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form');
+const formbtn = document.getElementById('form-button');
+const email = document.getElementById('email');
+const mainCard = document.getElementById('main-card');
+const successCard = document.getElementById('success-card');
+
+const dismissBtn = document.getElementById('btn-dismiss');
+
+let isEmailValid = false;
+
+formbtn.addEventListener('click' , (e) => {
+    if (!isEmailValid) {
+        email.classList.add('input-error');
+        form.classList.add('form-error');
+
+    } else {
+        email.classList.remove('input-error');
+        form.classList.remove('form-error');
+
+        mainCard.classList.add('no-display');
+        successCard.classList.remove('no-display');
+        email.value = "";
+        isEmailValid = false;
+    }
+
+    e.preventDefault();
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+dismissBtn.addEventListener('click', (e) => {
+    mainCard.classList.remove('no-display');
+    successCard.classList.add('no-display');
+})
+
+email.addEventListener('change', (e) => {
+    email.value = e.target.value;
+
+    isEmailValid = email.validity.valid && email.value !== '';
+})
+
